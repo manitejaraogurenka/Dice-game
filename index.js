ffe92e9c9d12e9f95b6af854b94d9895069dfde3
Maniteja Gurenka <140676670+ManitejaGurenka@users.noreply.github.com>
2023-08-13 10:13:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1;
-console.log(randomNumber1);
+function throwDice() {
+    var randomNumber1 = Math.floor(Math.random()*6)+1;
+    console.log(randomNumber1);
 
-var randomNumber2 = Math.floor(Math.random()*6)+1;
-console.log(randomNumber2);
+    var randomNumber2 = Math.floor(Math.random()*6)+1;
+    console.log(randomNumber2);
 
-document.querySelector(".img1").setAttribute("src", "dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "dice" + randomNumber2 + ".png");
+    document.querySelector(".img1").setAttribute("src", "dice" + randomNumber1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "dice" + randomNumber2 + ".png");
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "🚩Player1wins!";
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").textContent = "🚩Player1wins!";
+    }
+    else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").textContent = "Player2wins!🚩";
+    }
+    else {
+        document.querySelector("h1").textContent = "Draw!";
+    }
 }
-else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").textContent = "Player2wins!🚩";
-}
-else {
-    document.querySelector("h1").textContent = "Draw!";
-}
+
+document.onload = throwDice();
